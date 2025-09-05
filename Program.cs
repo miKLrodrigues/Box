dd03e2a65767c7d0614fd13bacdf7279397ce7ef
Mikaele <mikaelerodrigues07@outlook.com>
2025-09-05 15:57:24 -0300

    caixa concluida

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("+---------+");
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine("| Mikaele |");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("+---------+");
